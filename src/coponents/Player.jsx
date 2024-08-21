e32338da97d55043f4de9531023fc86a5acc9c57
borodkoleg <borodkoleg@gmail.com>
2024-08-21 16:06:44 +0300

    input_onChange_(Two-Way-Binding)_props_to_state

--- src/coponents/Player.jsx
+++ src/coponents/Player.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEdit = () => {
     setIsEditing((editing) => !editing);
-    setIsEditing((editing) => !editing);
+  }
+
+  const handleChangeName = (event) => {
+    setPlayerName(event.target.value);
   }
 
   return <li>
     <span className="player">
-      {!isEditing && <span className='player-name'>{name}</span>}
-      {isEditing && <input type="text" value={name} require />}
+      {!isEditing && <span className='player-name'>{playerName}</span>}
+      {isEditing && <input onChange={handleChangeName} type="text" value={playerName} require />}
       <span className='player-symbol'>{symbol}</span>
     </span>
     <button onClick={handleEdit}>{!isEditing ? 'Edit' : 'Save'}</button>
