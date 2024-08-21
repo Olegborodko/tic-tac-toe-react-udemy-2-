f0dfc644e6ded7d40d5c4c7ab6fe9de89456f7e7
borodkoleg <borodkoleg@gmail.com>
2024-08-21 21:01:05 +0300

    one state for two elements via parent

--- src/coponents/GameBoard.jsx
+++ src/coponents/GameBoard.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 
 const initialGameBoard = [
-  [null, 'X', '0'],
+  [null, null, null],
   [null, null, null],
   [null, null, null]
 ];
 
-export default function GameBoard({ initialName, symbol }) {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-      updatedBoard[rowIndex][colIndex] = 'X';
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare();
   }
 
   return <ol id="game-board">
